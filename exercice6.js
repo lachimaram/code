3222bf545f08985c6013fd911be34e337424e0f7
lachimaram <maram.lachi@esst-sup.com>
2026-06-12 13:54:40 +0100

    Add function to count vowels in a string

---
+++ exercice6.js
@@ -0,0 +1,16 @@
+function countVowels(str) {
+    let vowels = "aeiouAEIOU";
+    let count = 0;
+
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+let text = prompt("Enter a string:");
+
+console.log("Number of vowels:", countVowels(text));
