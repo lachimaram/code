78efaddf8ad151ef2175827deb8dbb86bd6ed730
lachimaram <maram.lachi@esst-sup.com>
2026-07-03 13:08:07 +0100

    Initialize Express server with routing for pages

---
+++ indexq.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+const PORT = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// Home Page
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "home.html"));
+});
+
+// About Page
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+
+// Contact Page
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "contact.html"));
+});
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
